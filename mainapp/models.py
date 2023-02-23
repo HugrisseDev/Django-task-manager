741ef14a6d44babd459e624733a0b2b5a9e4ede0
HugrisseDev <fusil.dawei@gmail.com>
2023-02-23 16:14:20 +0800

    postegresql database setting up

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=50)
-    content = models.CharField(max_length=200)
+    content = models.CharField(max_length=500)
     date_posted = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
