334d4ec6560e9434f680256f21b0c0a79611d244
HugrisseDev <fusil.dawei@gmail.com>
2023-02-26 01:35:06 +0800

    creating user registration form

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,9 +1,20 @@
 from django.forms import ModelForm
 from .models import Task
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class TaskForm(ModelForm):
     
     class Meta:
         model = Task
         fields = ("__all__")
+
+
+
+class Register(UserCreationForm):
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
