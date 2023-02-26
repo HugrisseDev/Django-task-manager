6975d9985d95da2d5863a866795af0a60224aaf0
HugrisseDev <fusil.dawei@gmail.com>
2023-02-26 23:55:13 +0800

    end for user registration code

--- mainapp/views.py
+++ mainapp/views.py
@@ -75,7 +75,7 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('taskForm')
         
     context = {'form' : form}
     return render(request, 'register.html', context = context)
