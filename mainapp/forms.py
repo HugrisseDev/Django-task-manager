fdd2fc1b9f4f11f0adb8de05045b9a50690d429a
HugrisseDev <fusil.dawei@gmail.com>
2023-02-27 20:14:43 +0800

    user login form.py updating

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -4,6 +4,9 @@ from .models import Task
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
+
 
 class TaskForm(ModelForm):
     
@@ -18,3 +21,8 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+        
+class Loginform(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
