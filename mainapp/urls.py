fadaa14a64448b280c20496c66f258fa98d57001
HugrisseDev <fusil.dawei@gmail.com>
2023-03-02 23:48:54 +0800

    user logout code

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path()
 ]
 
 
