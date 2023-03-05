ef8ffc338b3fb79f4f041d434da9ab68a7efeb30
HugrisseDev <fusil.dawei@gmail.com>
2023-03-05 12:16:59 +0800

    Task models code

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-
-    
+class Task(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    content = models.CharField(max_length=1000, null=True, blank=True)
+    date_posted = models.TimeField(auto_now=True, null=True)
