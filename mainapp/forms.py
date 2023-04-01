3b3863ec03cf7cb10f94b16cda781ae73defac13
HugrisseDev <fusil.dawei@gmail.com>
2023-04-02 02:00:50 +0800

    fixing some bugs

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -13,13 +13,19 @@ from .models import Task
 
 class CreateUserForm(UserCreationForm):
     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].label = 'Password'
+        self.fields['password2'].label = 'Password Confirmation'
+
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
         help_texts = {
             'username': None,
-            'password': None,
-            'password2': None,
         }
           
         
