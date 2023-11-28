38e184dab166fed9cb01e84c0c32205aeffefe3e
Hugrisse <hugrissedav@outlook.fr>
2023-11-28 23:57:39 +0800

    coming back

--- yiwuexpert/settings.py
+++ yiwuexpert/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     # added Apps
     # 'debug_toolbar',
     'mainapp',
-    'crispy_forms',
-    'crispy_bootstrap5',
+    # 'crispy_forms',
+    # 'crispy_bootstrap5',
 ]  
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
